4ef1acfea0e4a2626449854cd2ed0f10a99249c7
irumshaik19-lgtm <irumshaik19@gmail.com>
2025-12-28 15:33:28 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,9 @@
+function downloadVideo() {
+    const url = document.getElementById("url").value;
+    if (!url) {
+        alert("Enter a URL!");
+        return;
+    }
+    window.location.href = `/api/download?url=${encodeURIComponent(url)}`;
+}
 
